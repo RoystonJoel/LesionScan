89ee6ac67bbece2e6a195955fa52e326f61e4a16
Royston <roystonjoel.dev@gmail.com>
2026-09-14 13:11:06 +1200

    Main Activity

--- LesionScan/androidApp/src/main/kotlin/org/lesionscan/project/MainActivity.kt
+++ LesionScan/androidApp/src/main/kotlin/org/lesionscan/project/MainActivity.kt
@@ -3,23 +3,20 @@ package org.lesionscan.project
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import org.lesionscan.project.presentation.screens.CameraScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
         setContent {
-            App()
+            MaterialTheme {
+                Surface {
+                    CameraScreen(lifecycleOwner = this@MainActivity)
+                }
+            }
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
